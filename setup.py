cbe7a165fcf931d04af252762c07b5a6f98b6463
rAA7 <ri7nz.labs@gmail.com>
2017-10-28 12:17:40 +0700

    Fix Name Package

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-    name='distMsg',
+    name='distmsg',
     version='0.1',
     description='Send Notification To Service Message like Whatsapp, Slack, & More',
-    url='https://github.com/ri7nz/distMsg',
+    url='https://github.com/ri7nz/distmsg',
     author='ri7nz',
     author_email='ri7nz.labs@gmail.com',
     license='MIT',
