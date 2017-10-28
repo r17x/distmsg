471cc62554cf63801652a9755ab6929875955766
rAA7 <ri7nz.labs@gmail.com>
2017-10-28 11:56:21 +0700

    First Init Distribute  Message Python

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='distMsg',
+    version='0.1',
+    description='Send Notification To Service Message like Whatsapp, Slack, & More',
+    url='https://github.com/ri7nz/distMsg',
+    author='ri7nz',
+    author_email='ri7nz.labs@gmail.com',
+    license='MIT',
+    packages=['distMsg'],
+    zip_safe=False
+)
